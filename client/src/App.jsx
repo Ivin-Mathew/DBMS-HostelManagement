f02a13eda89d3e60c281c7e45fe41101092954d5
Rethin-2000 <165451650+Rethin-2000@users.noreply.github.com>
2024-10-11 10:24:56 +0530

    simple

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 
+import React from 'react';
+import UserHome from "./components/UserHome"
 function App() {
  
 
   return (
     <>
-      
+     <UserHome/>
     </>
-  )
+  );
 }
 
 export default App

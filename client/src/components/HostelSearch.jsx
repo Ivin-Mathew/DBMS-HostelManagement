b7c2273e3e61a0fa9bcb50ef5bae8d1ba4128c18
Sidharthan-B <xidharthan@gmail.com>
2024-10-13 20:04:45 +0530

    HosteSearch and Logo

--- client/src/components/HostelSearch.jsx
+++ client/src/components/HostelSearch.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 
 const HostelSearch = () => {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       {/* Sidebar */}
-      <div className="w-1/5 bg-blue-200 p-4">
-        <h1 className="text-2x1 font-bold mb-8">HOME</h1>
+      <div className="w-1/6 bg-blue-900 p-5 text-white">
+      <div className="flex items-center mb-6">
+          <img src="/logo.png" alt="Warden Dashboard Logo" className="h-24 w-24 mr-4" />
+        </div>
+        <h1 className="text-2x1 font-bold mb-8 text-white">HOME</h1>
         <div className="mb-4">
-          <h2 className="text-xl font-semibold">Filters</h2>
+          <h2 className="text-xl font-semibold text-white">Filters</h2>
           <div className="mt-4">
-            <label className="block text-sm font-medium text-gray-700">Budget</label>
+            <label className="block text-sm font-medium text-white">Budget</label>
             <input type="range" min="0" max="20000" className="w-full mt-2" />
-            <div className="flex justify-between text-sm text-gray-600">
+            <div className="flex justify-between text-white text-white">
               <span>₹0</span>
               <span>₹20000</span>
             </div>
           </div>
           <div className="mt-4">
-            <h3 className="text-lg font-medium">Room Type</h3>
+            <h3 className="text-white font-medium">Room Type</h3>
             <ul className="mt-2 space-y-2">
               <li>
                 <label className="inline-flex items-center">
@@ -43,12 +46,12 @@ const HostelSearch = () => {
       </div>
 
       {/* Main Content */}
-      <div className="w-3/4 p-4">
+      <div className="w-5/6 p-4">
         <div className="flex justify-between items-center mb-4">
           <input
             type="text"
             placeholder="Search"
-            className="w-2/3 p-2 border border-gray-300 rounded"
+            className="w-5/6 p-2 border border-gray-300 rounded"
           />
           <button className="p-2 bg-blue-500 text-blue rounded">Sort By Price</button>
         </div>

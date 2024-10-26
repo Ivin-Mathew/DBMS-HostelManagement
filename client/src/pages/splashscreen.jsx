1fd09d45c79f967694470ba0adc9c10e25e81b6d
Herogo3241 <goureesh.chandra23@gmail.com>
2024-10-26 10:35:18 +0530

    splash screen timeout change

--- client/src/pages/splashscreen.jsx
+++ client/src/pages/splashscreen.jsx
@@ -61,12 +61,12 @@ const SplashScreen = () => {
           className="fixed inset-0 bg-surface flex justify-center items-center"
           initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 0.8 }}
         >
           <motion.div
             initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ duration: 0.8, ease: "easeOut" }}
+            animate={{ scale: 1}}
+            transition={{ duration: 1, ease: "easeIn" }}
             className="text-center"
           >
             <HostigoAnimation />
